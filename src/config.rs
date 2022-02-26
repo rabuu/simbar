7f4435e9d13785d3c0af3a2f72887988a0117859
rabuu <rabuu.dev@gmail.com>
2022-02-26 01:52:47 +0100

    Implement CLI & config parsing

---
+++ src/config.rs
@@ -0,0 +1,51 @@
+use serde::Deserialize;
+
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+const DEFAULT_CONF_FILENAME: &str = "simbar.toml";
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub delimiter: String,
+    pub padding: bool,
+    pub modules: Vec<Module>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Module {
+    pub module: String,
+    pub repeat: Option<usize>,
+    pub fg: Option<String>,
+    pub bg: Option<String>,
+}
+
+impl Config {
+    pub fn new(config_path: Option<String>) -> Self {
+        let config_path = match config_path {
+            Some(config_path) => PathBuf::from_str(&config_path).expect("No valid path supplied"),
+            None => {
+                let xdg_dirs = xdg::BaseDirectories::with_prefix("simbar").unwrap();
+                xdg_dirs
+                    .find_config_file(DEFAULT_CONF_FILENAME)
+                    .unwrap_or_else(|| {
+                        panic!(
+                            "No configuration directory found, searched for: {}",
+                            DEFAULT_CONF_FILENAME
+                        );
+                    })
+            }
+        };
+
+        assert!(config_path.is_file(), "No file at: {:?}", config_path);
+
+        let mut modules_dir = config_path.clone();
+        modules_dir.pop();
+        modules_dir.push("modules/");
+        assert!(modules_dir.is_dir(), "No directory at: {:?}", modules_dir);
+
+        let config_str = fs::read_to_string(config_path).unwrap();
+        toml::from_str(&config_str).unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+mod config;
+
+use clap::{ArgGroup, Parser};
+use config::Config;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+#[clap(group(ArgGroup::new("out_mode").required(true).multiple(false)))]
+#[clap(group(ArgGroup::new("freq_mode").required(true).multiple(false)))]
+struct Cli {
+    #[clap(short, long, group = "out_mode")]
+    print: bool,
+    #[clap(short, long, group = "out_mode")]
+    xsetroot: bool,
+
+    #[clap(short, long, group = "freq_mode")]
+    once: bool,
+    #[clap(short, long, group = "freq_mode")]
+    repeat: bool,
+
+    #[clap(short, long)]
+    config: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let config = Config::new(cli.config);
+
+    println!("{:#?}", config);
 }
